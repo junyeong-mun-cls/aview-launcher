f5569e19d9a50b34c3ea7c617412e9547084791a
Junyeong Mun <junyeong.mun@corelinesoft.com>
2026-03-24 15:26:05 +0900

    aview - aviewhub 분리 구조

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -3,11 +3,23 @@ const { getLauncherStatus } = require("../services/statusService");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/aview", (req, res) => {
     const status = getLauncherStatus();
 
-    res.render("index", {
-        title: "AVIEW Launcher",
+    res.render("aview/index", {
+        title: "Aview Launcher",
+        currentBranch: status.currentBranch,
+        buildStatus: status.buildStatus,
+        appStatus: status.appStatus,
+        appUrl: status.appUrl,
+    });
+});
+
+router.get("/aviewhub", (req, res) => {
+    const status = getLauncherStatus();
+
+    res.render("aviewhub/index", {
+        title: "Aviewhub Launcher",
         currentBranch: status.currentBranch,
         buildStatus: status.buildStatus,
         appStatus: status.appStatus,
